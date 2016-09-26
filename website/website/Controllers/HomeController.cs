704c6fb11809ae456205e62e7f61cdadb9974453
marcotomas <m.ft.tomas@gmail.com>
2016-09-26 22:20:59 +0100

    shoppingcard add

--- website/website/Controllers/HomeController.cs
+++ website/website/Controllers/HomeController.cs
@@ -13,18 +13,9 @@ namespace website.Controllers
             return View();
         }
 
-        public ActionResult About()
+       public ActionResult addListCard()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return PartialView("shoppingCard");
         }
     }
 }
